34b7b22557e5cbecb40af9ac26a899f423255751
Rodrigo Fuenzalida <rfuenzalidac87@gmail.com>
2015-02-11 13:04:45 -0300

    removing unused files and adding a new method

--- index.js
+++ index.js
@@ -1,43 +1,65 @@
 'use strict';
 
-function Sanitize (value) {
-  this.str = value;
-}
-
-Sanitize.prototype.getValue = function () {
-  return this.str;
+/**
+ * delete extra space from text
+ * @param  {String} str [weird string]
+ * @return {String}     [cleaned string]
+ */
+var cleanString =  function (str) {
+  return str.replace(/[\s\r\t\n]/g, ' ').replace(/\s{2,}/g, ' ').trim();
+};
+/**
+ * Format String of number and transform it into Integer
+ * @param  {String} number [String of a number with random characters]
+ * @return {Integer}       [String only with number returned as Integer]
+ * i.e. 10.000 -> 10000
+ */
+var formatNumberNat = function (number) {
+  return parseInt(number.replace(/\D/g, ''), 10);
+};
+/**
+ * Format String of number and transform it into Float
+ * @param  {String} number [String of a number with random characters]
+ * @return {Float}         [String only with number returned as Float]
+ * i.e. 10.000,10 -> 10000.1
+ */
+var formatNumberInt = function (number) {
+  return parseFloat(number.replace(/\./g, '').replace(',', '.').replace(/[^\.^\d]/g, ''));
+};
+/**
+ * Format String of number and transform it into Float
+ * @param  {String} number [String of a number with random characters]
+ * @return {Float}         [String only with number returned as Float]
+ * i.e. 10,000.10 -> 10000.1
+ */
+var formatNumberInt2 = function (number) {
+  return parseFloat(number.replace(/,/g, '').replace(/[^\.^\d]/g, ''));
+};
+/**
+ * Capitalize a word
+ * @param  {String} str [random string]
+ * @return {String}     [Capitalized string]
+ */
+var capitalize = function (str) {
+  return str.charAt(0).toUpperCase() + str.substring(1).toLowerCase();
 };
 
-Sanitize.prototype.setValue = function (val) {
-  this.str = val;
+/**
+ * Capitalize a whole text
+ * @param  {String} phrase [random text]
+ * @return {String}        [Capitalized text]
+ */
+var phraseCapitalize = function (phrase) {
+  return phrase.split(' ').map(function (word) {
+    return capitalize(word);
+  }).join(' ');
 };
 
-Sanitize.prototype.cleanString = function () {
-  var value = this.getValue();
-  this.setValue(value.replace(/[^\wáéíóú]/g, ' ').replace(/(\s{2,})/g, ' ').trim());
+module.exports = {
+  'cleanString': cleanString,
+  'formatNumberNat': formatNumberNat,
+  'formatNumberInt': formatNumberInt,
+  'formatNumberInt2': formatNumberInt2,
+  'capitalize': capitalize,
+  'phraseCapitalize': phraseCapitalize
 };
-
-Sanitize.prototype.floatNumber = function () {
-  // i.e: $ 10.000,20
-  var value = this.getValue();
-  this.setValue(parseFloat(value.replace(/[\.\$\s]/g, '').replace(/,/, '.')));
-};
-
-Sanitize.prototype.floatNumber2 = function () {
-  // i.e: $ 10,000.20
-  var value = this.getValue();
-  this.setValue(parseFloat(value.replace(/[\,\$\s]/g, '')));
-};
-
-Sanitize.prototype.onlyNumber = function () {
-  // i.e: $ 10.000 -> 10000
-  var value = this.getValue();
-  this.setValue(parseFloat(value.replace(/[^\d]/g, '')));
-};
-
-Sanitize.prototype.Capitalize = function () {
-  var value = this.getValue();
-  this.setValue(value.charAt(0).toUpperCase() + value.slice(1).toLowerCase());
-};
-
-module.exports = Sanitize;
--- test/index.js
+++ test/index.js
@@ -6,59 +6,52 @@ describe('Sanitize', function(){
   describe('#clanString', function(){
     it('should return \'COMPRA NORMAL EN COMERCIO\' when use cleanString()', function(){
       var str = '\t\rCOMPRA\t\t\tNORMAL\t\t\r EN COMERCIO   \n';
-      var san = new Sanitize(str);
-      san.cleanString();
-      assert.equal('COMPRA NORMAL EN COMERCIO', san.getValue());
+      var res = Sanitize.cleanString(str);
+      assert.equal('COMPRA NORMAL EN COMERCIO', res);
     });
   });
   describe('#clanString', function(){
-    it('should return \'Traspaso Internet a T Crédito\' when use cleanString()', function(){
+    it('should return \'Traspaso Internet a T. Crédito\' when use cleanString()', function(){
       var str = 'Traspaso Internet a T. Crédito';
-      var san = new Sanitize(str);
-      san.cleanString();
-      assert.equal('Traspaso Internet a T Crédito', san.getValue());
+      var res = Sanitize.cleanString(str);
+      assert.equal('Traspaso Internet a T. Crédito', res);
     });
   });
 
   describe('#clanString', function(){
-    it('should return \'Traspaso con la Cuenta N 001010128176\' when use cleanString()', function(){
+    it('should return \'Traspaso con la Cuenta N° 001010128176\' when use cleanString()', function(){
       var str = 'Traspaso con la Cuenta N° 001010128176';
-      var san = new Sanitize(str);
-      san.cleanString();
-      assert.equal('Traspaso con la Cuenta N 001010128176', san.getValue());
+      var res = Sanitize.cleanString(str);
+      assert.equal('Traspaso con la Cuenta N° 001010128176', res);
     });
   });
   describe('#clanString', function(){
     it('should return \'Compra normal en comercio\' when use cleanString() and Capitalize()', function(){
       var str = '\t\rCOMPRA\t\t\tNORMAL\t\t\r (EN COMERCIO)\n';
-      var san = new Sanitize(str);
-      san.cleanString();
-      san.Capitalize();
-      assert.equal('Compra normal en comercio', san.getValue());
+      var res = Sanitize.cleanString(str);
+      res = Sanitize.capitalize(res);
+      assert.equal('Compra normal (en comercio)', res);
     });
   });
-  describe('#floatNumber', function(){
+  describe('#formatNumberInt', function(){
     var num = '10.000,20';
-    it('should return 10000.2 when use floatNumber() on ' + num, function(){
-      var san = new Sanitize(num);
-      san.floatNumber();
-      assert.equal(10000.2, san.getValue());
+    it('should return 10000.2 when use formatNumberInt() on ' + num, function(){
+      var res = Sanitize.formatNumberInt(num);
+      assert.equal(10000.2, res);
     });
   });
-  describe('#floatNumber2', function() {
+  describe('#formatNumberInt2', function() {
     var num = '10,000.20';
-    it('should return 10000.2 when use floatNumber2() on ' + num, function(){
-      var san = new Sanitize(num);
-      san.floatNumber2();
-      assert.equal(10000.2, san.getValue());
+    it('should return 10000.2 when use formatNumberInt2() on ' + num, function(){
+      var res = Sanitize.formatNumberInt2(num);
+      assert.equal(10000.2, res);
     });
   });
-  describe('#onlyNumber', function(){
+  describe('#formatNumberNat', function(){
     var num = '10.000.000';
-    it('should return 10000000 when use onlyNumber() on ' + num, function(){
-      var san = new Sanitize(num);
-      san.onlyNumber();
-      assert.equal(10000000, san.getValue());
+    it('should return 10000000 when use formatNumberNat() on ' + num, function(){
+      var res = Sanitize.formatNumberNat(num);
+      assert.equal(10000000, res);
     });
   });
 });
